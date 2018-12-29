05e09ed40dea730b47786452ab5e59425af6d0ee
ChristofDubs <christof.dubs@gmx.ch>
2018-12-29 15:02:50 +0100

    add context to test

--- tests/test.py
+++ tests/test.py
@@ -2,6 +2,7 @@ import unittest
 
 import numpy as np
 
+from context import pyrotation
 from pyrotation import Quaternion, quat_from_angle_axis, quat_from_angle_vector, quat_from_roll_pitch_yaw, rot_from_angle_axis, rot_from_angle_vector, rot_from_roll_pitch_yaw, rot_x, rot_y, rot_z, rot_to_roll_pitch_yaw
 
 
