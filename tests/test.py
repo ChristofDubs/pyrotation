f8ca20041f15c1a2d5a1c094e695091841e0ad02
ChristofDubs <christof.dubs@gmx.ch>
2018-12-29 17:46:57 +0100

    add comment for importing context

--- tests/test.py
+++ tests/test.py
@@ -2,7 +2,7 @@ import unittest
 
 import numpy as np
 
-import context
+import context  # lgtm [py/unused-import] (to resolve pyrotation if the package is not installed)
 from pyrotation import Quaternion, quat_from_angle_axis, quat_from_angle_vector, quat_from_roll_pitch_yaw, rot_from_angle_axis, rot_from_angle_vector, rot_from_roll_pitch_yaw, rot_x, rot_y, rot_z, rot_to_roll_pitch_yaw
 
 
