d470f00ae3017f28abed1eb7d1665b08940be56a
ChristofDubs <christof.dubs@gmx.ch>
2018-12-29 16:19:55 +0100

    define __all__ in init.py

--- pyrotation/__init__.py
+++ pyrotation/__init__.py
@@ -1,1 +1,14 @@
 from .rotation import *
+
+__all__ = [
+    'Quaternion',
+    'quat_from_angle_axis',
+    'quat_from_angle_vector',
+    'quat_from_roll_pitch_yaw',
+    'rot_from_angle_axis',
+    'rot_from_angle_vector',
+    'rot_from_roll_pitch_yaw',
+    'rot_x',
+    'rot_y',
+    'rot_z',
+    'rot_to_roll_pitch_yaw']
