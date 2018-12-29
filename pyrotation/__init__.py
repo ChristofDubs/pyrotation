1146944225dda484221b31c6f72353cc6a95c591
ChristofDubs <christof.dubs@gmx.ch>
2018-12-29 17:23:45 +0100

    explicitly import from .rotation

--- pyrotation/__init__.py
+++ pyrotation/__init__.py
@@ -1,4 +1,4 @@
-from .rotation import *
+from .rotation import Quaternion, quat_from_angle_axis, quat_from_angle_vector, quat_from_roll_pitch_yaw, rot_from_angle_axis, rot_from_angle_vector, rot_from_roll_pitch_yaw, rot_x, rot_y, rot_z, rot_to_roll_pitch_yaw
 
 __all__ = [
     'Quaternion',
